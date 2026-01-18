a498491fb1ac3711db1bf4d4a47a7ee783e72b67
Diego <diegogarciaangel25@gmail.com>
2026-01-18 22:22:23 +0100

    token

---
+++ middlewares/validarJWT.js
@@ -0,0 +1,23 @@
+import jwt  from 'jsonwebtoken'
+import {response, request} from 'express'  //Incorporamos esto aquí porque vamos a añadir elementos a req que sacaremos del token.
+
+export const validarJWT = (req , res , next) => {  //Estas asignaciones son necesarias para almacenar en el request los datos que extraigamos del token.
+    const token = req.header('x-token');  //Este nombre será establecido en el cliente también.
+
+    if (!token){
+        return res.status(401).json({'msg':'No hay token en la petición.'});
+    }
+
+    try {
+        
+        const {uid} = jwt.verify(token, process.env.SECRETORPRIVATEKEY);
+        req.dniToken = uid;
+        console.log(uid);
+        console.log(token);
+        next();
+        
+    }catch(error){
+        console.log(error);
+        res.status(401).json({'msg':'Token no válido.'});
+    }
+}
