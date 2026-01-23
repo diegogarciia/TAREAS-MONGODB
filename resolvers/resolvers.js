a6663eb7c24bf582bdd75bf3757844fb138344a3
Diego <diegogarciaangel25@gmail.com>
2026-01-23 23:43:42 +0100

    typeDefs + resolvers

---
+++ resolvers/resolvers.js
@@ -0,0 +1,82 @@
+import UserModel from '../models/Usuario.js';
+import TareaModel from '../models/Tarea.js';
+import { faker } from '@faker-js/faker';
+
+const resolvers = {
+    Query: {
+        usuarios: async () => {
+            return await UserModel.find();
+        },
+        usuario: async (_, { id }) => {
+            return await UserModel.findOne({ id });
+        },
+
+        tareas: async () => {
+            return await TareaModel.find();
+        },
+        tarea: async (_, { id }) => {
+            return await TareaModel.findOne({ id });
+        },
+        tareasPorUsuario: async (_, { userId }) => {
+            return await TareaModel.find({ idUsuarioAsignado: userId });
+        }
+    },
+
+    Mutation: {
+        
+        usuariosPost: async (_, { id, nombre, email, password, rol }) => {
+            const nuevoUsuario = new UserModel({ id, nombre, email, password, rol: rol || 'ESTANDAR' });
+            return await nuevoUsuario.save();
+        },
+
+        generarUsuariosAleatorios: async (_, { cantidad }) => {
+            const usuariosNuevos = [];
+            for (let i = 0; i < (cantidad || 10); i++) {
+                usuariosNuevos.push({
+                    id: Math.floor(Math.random() * 1000000),
+                    nombre: faker.person.fullName(),
+                    email: faker.internet.email(),
+                    password: faker.internet.password(),
+                    rol: 'ESTANDAR'
+                });
+            }
+            return await UserModel.insertMany(usuariosNuevos);
+        },
+
+        usuariosDelete: async (_, { id }) => {
+            const resultado = await UserModel.deleteOne({ id });
+            return resultado.deletedCount > 0;
+        },
+
+        agregarTarea: async (_, { idUsuarioAsignado, descripcion, duracion, dificultad, estado }) => {
+            const ultimaTarea = await TareaModel.findOne().sort('-id');
+            const nuevoId = ultimaTarea ? ultimaTarea.id + 1 : 1;
+
+            const nuevaTarea = new TareaModel({
+                id: nuevoId,
+                idUsuarioAsignado,
+                descripcion,
+                duracion,
+                dificultad,
+                estado: estado || 'Todo'
+            });
+
+            return await nuevaTarea.save();
+        },
+
+        tareasDelete: async (_, { id }) => {
+            const resultado = await TareaModel.deleteOne({ id });
+            return resultado.deletedCount > 0;
+        },
+
+        tareaActualizarEstado: async (_, { id, estado }) => {
+            return await TareaModel.findOneAndUpdate(
+                { id },
+                { estado },
+                { new: true }
+            );
+        }
+    }
+};
+
+export default resolvers;
---
+++ typeDefs/typeDefs.js
@@ -0,0 +1,55 @@
+import { gql } from 'graphql-tag';
+
+const typeDefs = gql`
+
+  type Usuario {
+    id: Int!
+    nombre: String!
+    email: String!
+    rol: String!
+  }
+
+  type Tarea {
+    id: Int!
+    idUsuarioAsignado: Int!
+    descripcion: String!
+    duracion: Int!
+    dificultad: String!
+    estado: String
+  }
+
+  type Query {
+    usuarios: [Usuario]
+    usuario(id: Int!): Usuario
+    tareas: [Tarea]
+    tarea(id: Int!): Tarea
+  }
+
+  type Mutation {    
+    usuariosPost(
+      id: Int!, 
+      nombre: String!, 
+      email: String!, 
+      password: String!, 
+      rol: String
+    ): Usuario
+
+    generarUsuariosAleatorios(cantidad: Int): [Usuario]
+
+    usuariosDelete(id: Int!): Boolean
+
+    agregarTarea(
+      idUsuarioAsignado: Int!, 
+      descripcion: String!, 
+      duracion: Int!, 
+      dificultad: String!, 
+      estado: String
+    ): Tarea
+
+    tareasDelete(id: Int!): Boolean
+
+    tareaActualizarEstado(id: Int!, estado: String!): Tarea
+  }
+`;
+
+export default typeDefs
