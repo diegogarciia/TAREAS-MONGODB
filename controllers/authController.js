525dfc0ab2ff112e51c58793dccd6bde691f7066
Diego <diegogarciaangel25@gmail.com>
2026-01-26 19:17:25 +0100

    login con google funcional

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ import { response } from 'express';
 import UserModel from '../models/Usuario.js';
 import { generarJWT_Roles, generarJWT } from '../helpers/generate_jwt.js'; 
 import { googleVerify } from '../helpers/google-verify.js';
+import { faker } from '@faker-js/faker';
 
 export const login = async (req, res = response) => {
     const { email, password } = req.body;  
@@ -25,20 +26,36 @@ export const login = async (req, res = response) => {
     }
 };
 
-export const googleSignin = async(req, res = response) => {
-
+export const googleSignin = async(req, res) => {
     const { id_token } = req.body;
     console.log(`Token recibido desde el cliente: ${id_token}`);
     
     try {
         const { correo, nombre, img } = await googleVerify( id_token );
 
-        console.log(`Comprobaríamos el usuario: ${correo}, ${nombre} ${img}`);
+        let usuario = await UserModel.findOne({ email: correo });
+
+        const ultimoUsuario = await UserModel.findOne().sort('-id');
+        const nuevoId = ultimoUsuario ? ultimoUsuario.id + 1 : 1;
+
+        if (!usuario) {
+            const data = {
+                id: nuevoId,
+                nombre,
+                email: correo,
+                img,
+                rol: 'ESTANDAR',
+                password: faker.internet.password()
+            };
+            usuario = new UserModel(data);
+            await usuario.save();
+        }
         
-        const token = generarJWT(correo); 
-        console.log(`Token JWT para consultar nuestro servicio generado: ${token}`);
-        res.status(200).json({correo, token, img});
-        
+        const token = await generarJWT(usuario.id);
+        res.status(200).json({
+            usuario, 
+            token    
+        });
     } catch (error) {
 
         res.status(400).json({
